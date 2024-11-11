81da749c5f0b8a71a40bacb6cb00e6e8987efce9
Ujjwal Ranjan Choubey <129257114+Ujjwal1267@users.noreply.github.com>
2024-11-11 20:49:31 +0530

    Update Register.jsx

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -23,7 +23,7 @@ const Register = () => {
     
       try {
         const res = await axios.post(
-          "http://localhost:4000/api/v1/user/patient/register",
+          "https://hospital-management3-backend.onrender.com/api/v1/user/patient/register",
           { firstName, lastName, email, phone, nic, dob, gender, password },
           {
             withCredentials: true,
